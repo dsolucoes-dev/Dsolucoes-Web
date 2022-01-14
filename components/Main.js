908998321096474ee3f5c059d8fd5705f929dc62
Carlos David <romao.carlosdavi@gmail.com>
2022-01-14 17:54:46 -0300

    layout started

---
+++ components/Main.js
@@ -0,0 +1,246 @@
+import {
+  MenuIcon,
+  XIcon,
+  AnnotationIcon,
+  GlobeAltIcon,
+  LightningBoltIcon,
+  ScaleIcon,
+} from "@heroicons/react/outline";
+
+const features = [
+  {
+    name: "Competitive exchange rates",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: GlobeAltIcon,
+  },
+  {
+    name: "No hidden fees",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: ScaleIcon,
+  },
+  {
+    name: "Transfers are instant",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: LightningBoltIcon,
+  },
+  {
+    name: "Mobile notifications",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: AnnotationIcon,
+  },
+];
+
+const features2 = [
+  { name: "Origin", description: "Designed by Good Goods, Inc." },
+  {
+    name: "Material",
+    description:
+      "Solid walnut base with rare earth magnets and powder coated steel card cover",
+  },
+  { name: "Dimensions", description: '6.25" x 3.55" x 1.15"' },
+  { name: "Finish", description: "Hand sanded and finished with natural oil" },
+  { name: "Includes", description: "Wood card tray and 3 refill packs" },
+  {
+    name: "Considerations",
+    description:
+      "Made from natural materials. Grain and color vary with each item.",
+  },
+];
+
+export default function Main() {
+  return (
+    <div class="py-12 bg-white">
+      <div class=" px-4 sm:px-6 lg:px-8 " id="teste">
+        <div class="lg:text-center">
+          <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+            Transactions
+          </h2>
+          <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            A better way to send money
+          </p>
+          <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
+            voluptatum cupiditate veritatis in accusamus quisquam.
+          </p>
+        </div>
+
+        <div class="mt-10">
+          <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg
+                    class="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                    />
+                  </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  Competitive exchange rates
+                </p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maiores impedit perferendis suscipit eaque, iste dolor
+                cupiditate blanditiis ratione.
+              </dd>
+            </div>
+
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg
+                    class="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+                    />
+                  </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  No hidden fees
+                </p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maiores impedit perferendis suscipit eaque, iste dolor
+                cupiditate blanditiis ratione.
+              </dd>
+            </div>
+
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg
+                    class="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M13 10V3L4 14h7v7l9-11h-7z"
+                    />
+                  </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  Transfers are instant
+                </p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maiores impedit perferendis suscipit eaque, iste dolor
+                cupiditate blanditiis ratione.
+              </dd>
+            </div>
+
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg
+                    class="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                    />
+                  </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  Mobile notifications
+                </p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maiores impedit perferendis suscipit eaque, iste dolor
+                cupiditate blanditiis ratione.
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+
+      <div className="bg-white">
+      <div className=" py-24 px-4 grid items-center grid-cols-1 gap-y-10 gap-x-8 sm:px-6 sm:py-32 xl:max-w-max lg:px-8 lg:grid-cols-2">
+        <div>
+        <h2 class="text-base text-indigo-600 mb-5 font-semibold tracking-wide uppercase">
+            Transactions
+          </h2>
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Technical Specifications</h2>
+          <p className="mt-4 text-gray-500">
+            The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated
+            steel divider separates active cards from new ones, or can be used to archive important task lists.
+          </p>
+
+          <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+            {features.map((feature) => (
+              <div key={feature.name} className="border-t border-gray-200 pt-4">
+                <dt className="font-medium text-gray-900">{feature.name}</dt>
+                <dd className="mt-2 text-sm text-gray-500">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+        <div className="hidden grid grid-cols-2 grid-rows-2 gap-4 lg:grid sm:gap-6 lg:gap-8 teste">
+          <img
+            src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
+            alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+            className=" rounded-lg p-10 "
+          />
+          <img
+            src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg"
+            alt="Top down view of walnut card tray with embedded magnets and card groove."
+            className=" rounded-lg p-10"
+          />
+          <img
+            src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg"
+            alt="Side of walnut card tray with card groove and recessed card area."
+            className=" rounded-lg p-10"
+          />
+          <img
+            src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg"
+            alt="Walnut card tray filled with cards and card angled in dedicated groove."
+            className=" rounded-lg p-10"
+          />
+        </div>
+      </div>
+    </div>
+
+    
+     
+    </div>
+  );
+}
