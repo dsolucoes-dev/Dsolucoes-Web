ba9e7315b93daa5d6b03521b07409988da4828ae
Carlos <romao.carlosdavi@gmail.com>
2022-01-14 21:33:39 -0300

    :rocket: pre deploy

--- components/Footer.js
+++ components/Footer.js
@@ -4,29 +4,28 @@ export default function Footer() {
       <div className="bg-white dark:bg-gray-800">
         <div className="relative px-4 py-6 overflow-hidden sm:px-6 sm:py-8 lg:p-12 xl:p-16">
           <h2 className="text-2xl font-semibold font-display text-black dark:text-white sm:text-3xl">
-            We&#x27;ve got more coming...
+            Vamos agilizar sua solução?
           </h2>
           <p className="mt-2 max-w-xl text-base text-gray-400">
-            Want to hear from us when we add new components? Sign up for our
-            newsletter and we&#x27;ll email you every time we release a new
-            batch of components.
+           Nós vamos adorar poder chama-lo de "Nosso Parceiro". Para agilizar o processo, preecha o formulário abaixo para entrar-mos em contato com você.
           </p>
           <form>
             <div className="sm:flex jusitfy-start mt-6">
               <form className="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
                 <div className=" relative ">
                   <input
-                    type="text"
+                    type="email"
+                    required
                     id='"form-subscribe-Subscribe'
-                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                    placeholder="Email"
+                    className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 md:ml-[20%]  w-60 p-5 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    placeholder="Coloque seu melhor e-mail"
                   />
                 </div>
                 <button
-                  className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
+                  className="flex-shrink-0 px-4 py-2  xl:ml-[50%]  w-60 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
                   type="submit"
                 >
-                  Subscribe
+                  Tornar-se parceiro
                 </button>
               </form>
             </div>
--- components/Main.js
+++ components/Main.js
@@ -9,27 +9,27 @@ import {
 
 const features = [
   {
-    name: "Competitive exchange rates",
+    name: "Desenvolvimento de Sites.",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      "Faça com que todos vejam suas ideias por meio de um site. Alcance novos públicos, leve sua ideia longe e expanda seus projetos conosco.",
     icon: GlobeAltIcon,
   },
   {
-    name: "No hidden fees",
+    name: "Desenvolvimento de Sistemas.",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      "Precisando gerenciar seu negócio? Crie conosco um sistema totalmente a sua cara. Desenvolvemos e atendemos todas as suas necessidades.",
     icon: ScaleIcon,
   },
   {
-    name: "Transfers are instant",
+    name: "Desenvolvimento de aplicativos.",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      "Qual tal levar aquela ideia fenomenal para os celulares de todos,hein!?Leve suas ideias para qualquer lugar e tenha seus projetos na palma da mão..",
     icon: LightningBoltIcon,
   },
   {
-    name: "Mobile notifications",
+    name: "Desenvolvimento de filtro p/ social midia.",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      "Entre nas redes sociais com uma ótima identidade visual. Crie um filtro para espalhar sua marca(com um pouco de humor ou não) e alcance mais engajamento.",
     icon: AnnotationIcon,
   },
 ];
@@ -53,156 +53,17 @@ const features2 = [
 
 export default function Main() {
   return (
-    <div className="py-12 bg-white">
-      <div className=" px-4 sm:px-6 lg:px-8 " id="teste">
-        <div className="lg:text-center">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-            Transactions
-          </h2>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            A better way to send money
-          </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-            voluptatum cupiditate veritatis in accusamus quisquam.
-          </p>
-        </div>
+    <div className="bg-white">
 
-        <div className="mt-10">
-          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-            <div className="relative">
-              <dt>
-                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
-                    />
-                  </svg>
-                </div>
-                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                  Competitive exchange rates
-                </p>
-              </dt>
-              <dd className="mt-2 ml-16 text-base text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Maiores impedit perferendis suscipit eaque, iste dolor
-                cupiditate blanditiis ratione.
-              </dd>
-            </div>
-
-            <div className="relative">
-              <dt>
-                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
-                    />
-                  </svg>
-                </div>
-                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                  No hidden fees
-                </p>
-              </dt>
-              <dd className="mt-2 ml-16 text-base text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Maiores impedit perferendis suscipit eaque, iste dolor
-                cupiditate blanditiis ratione.
-              </dd>
-            </div>
-
-            <div className="relative">
-              <dt>
-                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M13 10V3L4 14h7v7l9-11h-7z"
-                    />
-                  </svg>
-                </div>
-                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                  Transfers are instant
-                </p>
-              </dt>
-              <dd className="mt-2 ml-16 text-base text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Maiores impedit perferendis suscipit eaque, iste dolor
-                cupiditate blanditiis ratione.
-              </dd>
-            </div>
-
-            <div className="relative">
-              <dt>
-                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                    />
-                  </svg>
-                </div>
-                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                  Mobile notifications
-                </p>
-              </dt>
-              <dd className="mt-2 ml-16 text-base text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Maiores impedit perferendis suscipit eaque, iste dolor
-                cupiditate blanditiis ratione.
-              </dd>
-            </div>
-          </dl>
-        </div>
-      </div>
-
-      <div className="bg-white">
-        <div className=" py-24 px-4 grid items-center grid-cols-1 gap-y-10 gap-x-8 sm:px-6 sm:py-32 xl:max-w-max lg:px-8 lg:grid-cols-2">
+<div className="bg-white">
+        <div className=" py-10 px-4 grid items-center grid-cols-1 gap-y-10 gap-x-8 sm:px-6 sm:py-5 xl:max-w-max lg:px-8 lg:grid-cols-2">
           <div>
             <h2 className="text-base text-indigo-600 mb-5 font-semibold tracking-wide uppercase">
-              Transactions
+             <s> Nossos Serviços</s> <br /> Nossas soluções
             </h2>
-            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Technical Specifications</h2>
+            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Soluções que proporcionamos a você.</h2>
             <p className="mt-4 text-gray-500">
-              The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated
-              steel divider separates active cards from new ones, or can be used to archive important task lists.
+            Procuramos construir um ecossistema completo. Pensamos em diversas situações para que não você encontre tudo em um único lugar:
             </p>
 
             <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
@@ -238,6 +99,141 @@ export default function Main() {
           </div>
         </div>
       </div>
+
+
+
+
+      <div className=" px-4 sm:px-6 lg:px-8 ">
+        <div className="lg:text-center">
+          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+            Fábrica de ideias 
+          </h2>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          Somos os melhores para alavancar seus projetos.
+          </p>
+          <p className="mt-4 text-gray-500">
+          Chegou a hora de tirar os projetos do papel e colocar em prática conosco! Proporcionamos a melhor experiência, do início ao fim. Oferecemos ao cliente:
+            </p>
+        </div>
+
+        <div className="mt-10 pb-10 pt-5">
+          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+            <div className="relative">
+              <dt>
+                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg
+                    className="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                    />
+                  </svg>
+                </div>
+                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  Atendimento Personalizado
+                </p>
+              </dt>
+              <dd className="mt-2 ml-16 text-base text-gray-500">
+              Nosso time de atendimento é altamente capacitado para atender e esclarecer quaisquer dúvidas e ajudar em diversas situações.
+              </dd>
+            </div>
+
+            <div className="relative">
+              <dt>
+                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg
+                    className="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+                    />
+                  </svg>
+                </div>
+                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  Garantia de Entrega:
+                </p>
+              </dt>
+              <dd className="mt-2 ml-16 text-base text-gray-500">
+              Somos totalmente transparentes em nossos projetos para com prazos e nos empenhamos 110% para com o bem-estar de nossos clientes.🚀
+              </dd>
+            </div>
+
+            <div className="relative">
+              <dt>
+                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg
+                    className="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M13 10V3L4 14h7v7l9-11h-7z"
+                    />
+                  </svg>
+                </div>
+                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                Suporte Campeão
+                </p>
+              </dt>
+              <dd className="mt-2 ml-16 text-base text-gray-500">
+              Nossa equipe de suporte está sempre a postos para agilizar a resolução de qualquer contratempo e garantir seu sucesso.
+              </dd>
+            </div>
+
+            <div className="relative">
+              <dt>
+                <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg
+                    className="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                    />
+                  </svg>
+                </div>
+                <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                Satisfação completa.
+                </p>
+              </dt>
+              <dd className="mt-2 ml-16 text-base text-gray-500">
+              Satisfação completa. Garantimos todos esses pontos anteriores por um único objetivo: Assegurar sua satisfação e bem estar com nossos serviços. Pois sabemos que: Seu sucesso também é nosso
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+
+      
 
 
 
